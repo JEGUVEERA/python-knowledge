65632e9d13c44af089eb95aea9bb470aa374840f
jeguveera <jagaddisshmediboina561@gmail.com>
2025-01-24 17:10:56 +0530

    adde readme

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def hello (jaggu) :
      return "Hello World!,{jaggu}"
-print (hello(" "))
+print ("hello ")
 
- 
+jaggu = "jaggu"
+mounika = "mounika"
+#  echo "# python-knowledge" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/JEGUVEERA/python-knowledge.git
+# git push -u origin main
