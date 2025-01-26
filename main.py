048357a603de6af1cf6e06bbf22ad6d0675404aa
jeguveera <jagaddisshmediboina561@gmail.com>
2025-01-26 13:12:14 +0530

    update

--- main.py
+++ main.py
@@ -3,17 +3,17 @@
 # print ("(hello , tarun"),("hello , jaggu"),("hello , mounika")   
 
 
-for i in range(10):
-     print(i)
-     if i == 5:
-          break 
-     else:
-          print("done")
+# for i in range(10):
+#      print(i)
+#      if i == 5:
+#           break 
+#      else:
+#           print("done")
 
 
-tarun = "tarun"
-jaggu = "jaggu"
-mounika = "mounika"
+# tarun = "tarun"
+# jaggu = "jaggu"
+# mounika = "mounika"
 
 #  echo "# python-knowledge" >> README.md
 # git init
@@ -22,3 +22,16 @@ mounika = "mounika"
 # git branch -M main
 # git remote add origin https://github.com/JEGUVEERA/python-knowledge.git
 # git push -u origin main
+
+
+def ticket(age):
+     if age <= 3:
+          return "10"
+     elif age <= 10:
+          return "15"
+     elif age <= 60:
+          return "25"
+     else:
+          return "30"
+     
+print(ticket(4))
